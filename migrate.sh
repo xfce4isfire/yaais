8bc04324933c411abaca320d419013ee149226be
xfce4isfire <ergimamiaron@gmail.com>
2025-07-21 12:12:54 +0300

    Update migrate.sh

--- migrate.sh
+++ migrate.sh
@@ -6,12 +6,10 @@ sleep 1
 echo "This script assumes you have set SigLevel = Never in /etc/pacman.conf"
 sleep 5
 
-# Backup configs
 mv -vf /etc/pacman.conf /etc/pacman.conf.arch
 mv -vf /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist-arch
 sleep 1
 
-# Download Artix configs
 curl -L https://gitea.artixlinux.org/packages/pacman/raw/branch/master/pacman.conf -o /etc/pacman.conf
 curl -L https://gitea.artixlinux.org/packages/artix-mirrorlist/raw/branch/master/mirrorlist -o /etc/pacman.d/mirrorlist
 cp -vf /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.artix
@@ -23,11 +21,9 @@ pacman -S artix-keyring --noconfirm
 pacman-key --populate artix
 pacman-key --lsign-key 95AEC5D0C1E294FC9F82B253573A673A53C01BC2
 
-# Clean and refresh
 pacman -Syy
 sleep 2
 
-# Pre-download essential packages
 pacman -Sw --noconfirm \
   base base-devel grub linux linux-headers mkinitcpio \
   rsync lsb-release esysusers etmpfiles artix-branding-base \
@@ -65,15 +61,12 @@ pacman -Sl world  | grep installed | cut -d" " -f2 | pacman -S --noconfirm -
 pacman -Sl galaxy | grep installed | cut -d" " -f2 | pacman -S --noconfirm -
 sleep 2
 
-# Add init scripts
 pacman -S --needed --noconfirm acpid-openrc alsa-utils-openrc cronie-openrc cups-openrc fuse-openrc haveged-openrc hdparm-openrc openssh-openrc samba-openrc syslog-ng-openrc
 
-# Enable services
 for svc in acpid alsasound cronie cupsd sddm fuse haveged hdparm smb sshd syslog-ng udev; do
   rc-update add "$svc" default
 done
 
-# Networking
 pacman -S --needed --noconfirm netifrc
 echo 'nameserver 1.1.1.1' > /etc/resolv.conf
 echo 'GRUB_CMDLINE_LINUX="net.ifnames=0"' >> /etc/default/grub
@@ -81,17 +74,14 @@ ln -sf /etc/init.d/net.lo /etc/init.d/net.eth0
 rc-update add net.eth0 boot
 rc-update add udev sysinit boot
 
-# Remove systemd users
 for usr in journal journal-gateway timesync network bus-proxy journal-remote journal-upload resolve coredump; do
   userdel "systemd-$usr" 2>/dev/null || true
 done
 rm -rf /etc/systemd /var/lib/systemd
 
-# Cleanup
 sed -i '/init=\/usr\/lib\/systemd\/systemd/d' /etc/default/grub
 sed -i '/x-systemd/d' /etc/fstab
 
-# Regenerate initramfs
 mkinitcpio -P
 
 pacman -S --noconfirm lightdm lightdm-gtk-greeter
